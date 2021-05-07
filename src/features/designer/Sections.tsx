55470318f05e640e55091ac6c5d11a13344320c6
Luis Rodriguez <luisrodge@outlook.com>
2021-05-07 08:47:22 -0600

    Minor refactor

--- src/features/designer/Sections.tsx
+++ src/features/designer/Sections.tsx
@@ -1,7 +1,22 @@
-import Section from "../../components/Section";
+import styled from "styled-components";
+import { blue } from "@ant-design/colors";
+
 import Rows from "./Rows";
 
 import { ISection } from "./designer.interface";
+
+const SectionContainer = styled.div`
+  text-align: center;
+  padding: 30px 0;
+  border: 1px solid transparent;
+  border-bottom: 1px solid #f0f0f0;
+  border-radius: 2px;
+  cursor: pointer;
+  :hover {
+    border-bottom: 0;
+    border: 1px dashed ${blue.primary};
+  }
+`;
 
 interface ISections {
   sections: ISection[];
@@ -12,9 +27,9 @@ const Sections = ({ sections, setOpenElementDrawer }: ISections) => {
   return (
     <>
       {sections.map((section) => (
-        <Section key={section.id}>
+        <SectionContainer key={section.id}>
           <Rows sectionId={section.id} />
-        </Section>
+        </SectionContainer>
       ))}
     </>
   );
