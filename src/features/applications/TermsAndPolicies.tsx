d508c0c7ed97046ed4e97f72c9888ae452dad4ae
Luis Rodriguez <luisrodge@outlook.com>
2021-06-02 22:02:26 -0600

    Minor update

--- src/features/applications/TermsAndPolicies.tsx
+++ src/features/applications/TermsAndPolicies.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Typography, message, Divider } from "antd";
 import { useParams } from "react-router";
 import styled from "styled-components";
