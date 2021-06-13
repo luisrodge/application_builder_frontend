69d44d4ed957d79e212ddb30735091106822583e
Luis Rodriguez <luisrodge@outlook.com>
2021-06-12 23:19:17 -0600

    slogan update

--- src/components/SimpleHome.tsx
+++ src/components/SimpleHome.tsx
@@ -28,7 +28,7 @@ export default function SimpleHome() {
           <span style={{ color: blue.primary }}>Quik</span>apply
         </Title>
         <Text style={{ margin: 0, color: grey.primary, fontSize: 16 }}>
-          Bring your printed <FileTextOutlined /> applications online - the{" "}
+          Bring your printed <FileTextOutlined /> application forms online - the{" "}
           <strong>quik</strong> way!
         </Text>
         <Button
